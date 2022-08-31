7e133373906a2ee5cac0103dad5257e1eec2b0fb
Benjamin Shen <bfs45@cornell.edu>
2022-08-31 10:20:04 -0400

    init

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-
-import "bootstrap/dist/css/bootstrap.min.css"
-
-const addScript = url => {
-  const script = document.createElement("script")
-  script.src = url
-  script.async = true
-  document.body.appendChild(script)
-}
-
-export const onClientEntry = () => {
-  window.onload = () => {}
-}
+// custom typefaces
+import 'typeface-montserrat';
+import 'typeface-merriweather';
+// normalize CSS across browsers
+import './src2/src/normalize.css';
